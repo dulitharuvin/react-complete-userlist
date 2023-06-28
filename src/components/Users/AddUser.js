34b60d89cf4fb62a9b87323deafdd8ce9522d513
dulitharuvin <dulitha.ruvin@gmail.com>
2023-06-29 00:16:11 +0200

    error modal showing dynamically on input errors

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../Utils/ErrorModal";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -9,21 +10,32 @@ const AddUser = (props) => {
 
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  const [ivalidFormData, setInvalidFormData] = useState(true);
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
-    if (!enteredUsername.trim().length === 0 || +enteredAge < 1) {
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non empty values).",
+      });
       return;
     }
+
+    if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
+    }
+
     const newUser = {
       id: Math.random().toString(),
       name: enteredUsername,
       age: enteredAge,
     };
-
-    setInvalidFormData(false);
     onAddUser(newUser);
     setEnteredUsername("");
     setEnteredAge("");
@@ -37,26 +49,40 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const okayHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={userNameChangeHandler}
-          value={enteredUsername}
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onOkay={okayHandler}
         />
-        <label htmlFor="age">Age (Years)</label>
-        <input
-          id="age"
-          type="number"
-          onChange={ageChangeHandler}
-          value={enteredAge}
-        />
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+      )}
+
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={userNameChangeHandler}
+            value={enteredUsername}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            onChange={ageChangeHandler}
+            value={enteredAge}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
--- src/components/Utils/ErrorModal.js
+++ src/components/Utils/ErrorModal.js
@@ -1,18 +1,30 @@
 import React from "react";
 
-import classes from "./Card.module.css";
+import classes from "./ErrorModal.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const ErrorModal = (props) => {
+  const { title, message, onOkay } = props;
+
+  const okHandler = () => {
+    onOkay();
+  };
+
   return (
-    <div className={classes.modal}>
-      <div className={classes.header}>
-        <h2>Erro</h2>
-      </div>
-      <div className={classes.content}></div>
-      <div className={classes.actions}>
-        <button>Cancel</button>
-        <button>Ok</button>
-      </div>
+    <div>
+      <div className={classes.backdrop}></div>
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{message}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button onClick={okHandler}>Okay</Button>
+        </footer>
+      </Card>
     </div>
   );
 };
