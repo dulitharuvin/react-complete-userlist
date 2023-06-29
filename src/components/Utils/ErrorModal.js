8f1d88a32ce908e3d8cdac31c3c905ca67a01142
dulitharuvin <dulitha.ruvin@gmail.com>
2023-06-29 12:50:55 +0200

    using createPortal in react dom to portal an element to the place we want

--- src/components/Utils/ErrorModal.js
+++ src/components/Utils/ErrorModal.js
@@ -1,31 +1,48 @@
 import React from "react";
+import ReactDom from "react-dom";
 
 import classes from "./ErrorModal.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
+const Backdrop = (props) => {
+  const { onConfirm } = props;
+
+  return <div className={classes.backdrop} onClick={onConfirm}></div>;
+};
+
+const ModalOverlay = (props) => {
+  const { title, message, onConfirm } = props;
+
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   const { title, message, onConfirm } = props;
 
-  const onConfirmHandler = () => {
-    onConfirm();
-  };
-
   return (
-    <div>
-      <div className={classes.backdrop} onClick={onConfirmHandler}></div>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={onConfirmHandler}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onConfirm={onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDom.createPortal(
+        <ModalOverlay title={title} message={message} onConfirm={onConfirm} />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
